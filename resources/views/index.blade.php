cd0bda809db05102e071a45b656e50f14edf1cad
Parham Alvani <parham.alvani@gmail.com>
2016-06-16 17:49:58 +0430

    Login page was improved.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,38 +1,38 @@
 @extends('layout.app')
 @section('content')
-	<div id="container">
-	<div class="forms">
-		<form id="registeration">
-			<div class="titles">
-				<h5>First Name :</h5>
-				<h5>Last Name :</h5>
-				<h5>Email :</h5>
-				<h5>Password :</h5>
-				<h5>Image :</h5>
-				<input type="submit" value="Register" name="Register">
-			</div>
-			<div class="inputs">
-				<input type="text" name="firstname">
-				<input type="text" name="lastname">
-				<input type="email" name="email">
-				<input type="password" name="pass">
-				<input type="file" name="image">
-			</div>
-		</form>
-	</div>
-	<div class="forms">
-		<form id="login">
-			<div class="titles">
-				<h5>Email :</h5>
-				<h5>Password :</h5>
-				<input type="checkbox" id="checkremember"> <h6>Remember me</h6>
-				<input type="submit" value="Login" name="Login">
-			</div>
-			<div class="inputs">
-				<input type="email" name="email">
-				<input type="password" name="pass">
-			</div>
-		</form>
-	</div>
+	<div class="container">
+	<form id="signup">
+		<div class="titles">
+			<h5>First Name :</h5>
+			<h5>Last Name :</h5>
+			<h5>Email :</h5>
+			<h5>Password :</h5>
+			<h5>Image :</h5>
+			<input type="submit" value="Register" name="Register">
+		</div>
+		<div class="inputs">
+			<input type="text" name="firstname">
+			<input type="text" name="lastname">
+			<input type="email" name="email">
+			<input type="password" name="pass">
+			<input type="file" name="image">
+		</div>
+	</form>
+	<form id="login">
+		<div class="form-group">
+			<label for="email">Email address</label>
+			<input type="email" class="form-control" placeholder="Email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" class="form-control" placeholder="Password" name="password">
+		</div>
+		<div class="checkbox">
+			<label>
+				<input type="checkbox" id="checkremember">Remember me
+			</label>
+		</div>
+		<button type="submit" class="btn btn-default" value="Login" name="Login">Login</button>
+	</form>
 </div>
 @endsection
