7851173bd55ff8b00174d8707184837c271ca42c
Parham Alvani <parham.alvani@gmail.com>
2016-06-17 07:13:22 +0430

    Improve register form.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,22 +4,29 @@
 	<div class="page-header">
 		<h1>TMail <small>Simple way to destroy your mails :D</small></h1>
 	</div>
-	<form id="signup">
-		<div class="titles">
-			<h5>First Name :</h5>
-			<h5>Last Name :</h5>
-			<h5>Email :</h5>
-			<h5>Password :</h5>
-			<h5>Image :</h5>
-			<input type="submit" value="Register" name="Register">
+	<form method="post">
+		<div class="form-group">
+			<label for="firstname">First Name</label>
+			<input type="text" class="form-control" placeholder="First Name" name="firstname">
 		</div>
-		<div class="inputs">
-			<input type="text" name="firstname">
-			<input type="text" name="lastname">
-			<input type="email" name="email">
-			<input type="password" name="pass">
+		<div class="form-group">
+			<label for="lastname">Last Name</label>
+			<input type="text" class="form-control" placeholder="Last Name" name="lastname">
+		</div>
+		<div class="form-group">
+			<label for="email">Email</label>
+			<input type="email" class="form-control" placeholder="Email" name="email">
+		</div>
+		<div class="form-group">
+			<label for="password">Password</label>
+			<input type="password" class="form-control" placeholder="Password" name="password">
+		</div>
+		<div class="form-group">
+			<label for="image">Profile Picture</label>
 			<input type="file" name="image">
+			<p class="help-block">Please upload jpg file</p>
 		</div>
+		<button type="submit" class="btn btn-default" name="Register">Register</button>
 	</form>
 	<form action="<?php echo route('login'); ?>" method="post">
 		{!! csrf_field() !!}
@@ -36,7 +43,7 @@
 				<input type="checkbox" id="checkremember">Remember me
 			</label>
 		</div>
-		<button type="submit" class="btn btn-default" value="Login" name="Login">Login</button>
+		<button type="submit" class="btn btn-default" name="Login">Login</button>
 	</form>
 </div>
 @endsection
