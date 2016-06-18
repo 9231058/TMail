e0d1d02832efae6121fd9a3dec68be7d5705c194
Parham Alvani <parham.alvani@gmail.com>
2016-06-19 01:31:42 +0430

    Index file now have block for forms :)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,46 +4,53 @@
 	<div class="page-header">
 		<h1>TMail <small>Simple way to destroy your mails :D</small></h1>
 	</div>
-	<form method="post">
-		<div class="form-group">
-			<label for="firstname">First Name</label>
-			<input type="text" class="form-control" placeholder="First Name" name="firstname">
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<form id="register" method="post">
+				<div class="form-group">
+					<label for="firstname">First Name</label>
+					<input type="text" class="form-control" placeholder="First Name" name="firstname">
+				</div>
+				<div class="form-group">
+					<label for="lastname">Last Name</label>
+					<input type="text" class="form-control" placeholder="Last Name" name="lastname">
+				</div>
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" placeholder="Email" name="email">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" placeholder="Password" name="password">
+				</div>
+				<div class="form-group">
+					<label for="image">Profile Picture</label>
+					<input type="file" name="image">
+					<p class="help-block">Please upload jpg file</p>
+				</div>
+				<button type="submit" class="btn btn-default" name="Register">Register</button>
+			</form>
+			<form id="login" action="<?php echo route('login'); ?>" method="post">
+				{!! csrf_field() !!}
+				<div class="form-group">
+					<label for="email">Email address</label>
+					<input type="email" class="form-control" placeholder="Email" name="email">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" placeholder="Password" name="password">
+				</div>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" id="checkremember">Remember me
+					</label>
+				</div>
+				<button type="submit" class="btn btn-default" name="Login">Login</button>
+			</form>
+			<p class="text-center">
+			<a id="register-btn">Register New Account</a>
+			</p>
 		</div>
-		<div class="form-group">
-			<label for="lastname">Last Name</label>
-			<input type="text" class="form-control" placeholder="Last Name" name="lastname">
-		</div>
-		<div class="form-group">
-			<label for="email">Email</label>
-			<input type="email" class="form-control" placeholder="Email" name="email">
-		</div>
-		<div class="form-group">
-			<label for="password">Password</label>
-			<input type="password" class="form-control" placeholder="Password" name="password">
-		</div>
-		<div class="form-group">
-			<label for="image">Profile Picture</label>
-			<input type="file" name="image">
-			<p class="help-block">Please upload jpg file</p>
-		</div>
-		<button type="submit" class="btn btn-default" name="Register">Register</button>
-	</form>
-	<form action="<?php echo route('login'); ?>" method="post">
-		{!! csrf_field() !!}
-		<div class="form-group">
-			<label for="email">Email address</label>
-			<input type="email" class="form-control" placeholder="Email" name="email">
-		</div>
-		<div class="form-group">
-			<label for="password">Password</label>
-			<input type="password" class="form-control" placeholder="Password" name="password">
-		</div>
-		<div class="checkbox">
-			<label>
-				<input type="checkbox" id="checkremember">Remember me
-			</label>
-		</div>
-		<button type="submit" class="btn btn-default" name="Login">Login</button>
-	</form>
+	</div>
 </div>
 @endsection
