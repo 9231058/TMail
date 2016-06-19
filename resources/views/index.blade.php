a3ce12720a0a0cac909864ee77203da3cd206b43
Parham Alvani <parham.alvani@gmail.com>
2016-06-19 10:22:11 +0430

    Improve file code style :)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 	<div class="container">
 	<div class="page-header">
-		<h1>TMail <small>Simple way to destroy your mails :D</small></h1>
+        <h1>TMail <small>Simple way to destroy your mails :D</small></h1>
 	</div>
 	<div class="panel panel-default">
 		<div class="panel-body">
@@ -31,26 +31,26 @@
 				<button type="submit" class="btn btn-default" name="Register">Register</button>
 			</form>
 			<form id="login" action="<?php echo route('login'); ?>" method="post">
-				{!! csrf_field() !!}
-				<div class="form-group">
-					<label for="email">Email address</label>
-					<input type="email" class="form-control" placeholder="Email" name="email">
-				</div>
-				<div class="form-group">
-					<label for="password">Password</label>
-					<input type="password" class="form-control" placeholder="Password" name="password">
-				</div>
-				<div class="checkbox">
-					<label>
-						<input type="checkbox" id="checkremember">Remember me
-					</label>
-				</div>
-				<button type="submit" class="btn btn-default" name="Login">Login</button>
-			</form>
-			<p class="text-center">
-			<a id="register-btn">Register New Account</a>
-			</p>
-		</div>
-	</div>
+                {!! csrf_field() !!}
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" placeholder="Email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" placeholder="Password" name="password">
+                </div>
+                <div class="checkbox">
+                    <label>
+                        <input type="checkbox" id="checkremember">Remember me
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-default" name="Login">Login</button>
+            </form>
+            <p class="text-center">
+            <a id="register-btn">Register New Account</a>
+            </p>
+        </div>
+    </div>
 </div>
 @endsection
