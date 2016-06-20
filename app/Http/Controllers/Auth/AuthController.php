ad3faef27a90a273a71f4136ac7ae665a956f91b
Parham Alvani <parham.alvani@gmail.com>
2016-06-20 14:34:18 +0430

    Authentication controller was improved.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -50,8 +50,10 @@ class AuthController extends Controller
     protected function validator(array $data)
     {
         return Validator::make(
-            $data, [
-            'name' => 'required|max:255',
+            $data,
+            [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
             ]
@@ -61,7 +63,7 @@ class AuthController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param array $data user data
+     * @param array $data validated user data
      *
      * @return User
      */
@@ -69,7 +71,8 @@ class AuthController extends Controller
     {
         return User::create(
             [
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
             ]
