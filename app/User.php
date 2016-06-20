ba978420bb098245b34cd7c85f79628db44f8fb0
Parham Alvani <parham.alvani@gmail.com>
2016-06-20 09:13:23 +0430

    Correct user fillable array.

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'fistName', 'lastName', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     /**
