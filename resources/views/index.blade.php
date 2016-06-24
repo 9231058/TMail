73e97929692fc63e6d25c81f575d053f8ef27760
Parham Alvani <parham.alvani@gmail.com>
2016-06-24 22:30:10 +0430

    Register form add by include :)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,31 +7,7 @@
     </div>
     <div class="panel panel-default">
         <div class="panel-body">
-            <form id="register" action={{ url('/register') }} method="post">
-                {!! csrf_field() !!}
-                <div class="form-group">
-                    <label for="first_name">First Name</label>
-                    <input type="text" class="form-control" placeholder="First Name" name="first_name">
-                </div>
-                <div class="form-group">
-                    <label for="last_name">Last Name</label>
-                    <input type="text" class="form-control" placeholder="Last Name" name="last_name">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" placeholder="Email" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" placeholder="Password" name="password">
-                </div>
-                <div class="form-group">
-                    <label for="image">Profile Picture</label>
-                    <input type="file" name="image">
-                    <p class="help-block">Please upload jpg file</p>
-                </div>
-                <button type="submit" class="btn btn-default" name="Register">Register</button>
-            </form>
+            @include('forms.register')
             @include('forms.login')
             <p class="text-center">
             <a id="register-btn">Register New Account</a>
