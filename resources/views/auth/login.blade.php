f823a4303d45db0e5b3629aa5046880f673a9774
Parham Alvani <parham.alvani@gmail.com>
2016-06-24 22:26:02 +0430

    Login page was improved.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,5 +3,14 @@
     Login
 @endsection
 @section('content')
-    @include('forms.login')
+<div class="container">
+    <div class="page-header">
+        <h1>TMail <small>Simple way to destroy your mails :D</small></h1>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            @include('forms.login')
+        </div>
+    </div>
+</div>
 @endsection
