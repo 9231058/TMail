abaaa3acb187a96e364204ac7ac4806dc3939dc2
Parham Alvani <parham.alvani@gmail.com>
2016-06-24 20:29:21 +0430

    HTML and Forms service provider was added.

--- config/app.php
+++ config/app.php
@@ -151,6 +151,11 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
+         * Laravel Collective Service Providers...
+         */
+        Collective\Html\HtmlServiceProvider::class,
+
+        /*
          * Application Service Providers...
          */
         TMail\Providers\AppServiceProvider::class,
@@ -203,6 +208,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
