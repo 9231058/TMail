f16fb78d6a0590e31374293961890248cda9c833
Parham Alvani <parham.alvani@gmail.com>
2016-06-26 03:03:02 +0430

    Alias for Captcha was removed.

--- config/app.php
+++ config/app.php
@@ -215,7 +215,6 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Captcha' => Anhskohbo\NoCaptcha\NoCaptcha::class,
     ],
 
 ];
