3b387753c6c3e4cc00f55dd32c739ea8ff667efb
Parham Alvani <parham.alvani@gmail.com>
2016-06-26 03:54:02 +0430

    Inbox page was improved.

--- resources/views/inbox.blade.php
+++ resources/views/inbox.blade.php
@@ -1,239 +1,124 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>IE Project 4</title>
-    <link rel="stylesheet" type="text/css" href="../Stylesheets/Inbox.css">
-</head>
-<body>
-<div id="header">
-<h1>Inbox</h1>
-</div>
-
-<div id="box">
-    <h5 id="refresh">Refresh</h5>
-    <h5 id="compose">Compose</h5>
-    <h5 id="inbox">Inbox</h5>
-    <h5 id="sent">Sent</h5>
-    Num of Mail:<input type="text" name="numOfMail" id="numOfMail" value="4"><br>
-    <input type="radio" name="sortby" id="date" checked="checked" value="date">Sort By Date<br>
-    <input type="radio" name="sortby" id="sender" checked="checked" value="sender">Sort By sender<br>
-    <input type="radio" name="sortby" id="attach" checked="checked" value="attach">Sort By attachment<br>
-
-</div>
-<div id="mails">
-    <div class="eachMail">
-        <div class="from">ali</div>
-        <div class="subject">hello!</div>
-        <div class="text">this is my first email...</div>
-        <div class="date">3:02 95/4/3</div>
+@extends('layouts.app')
+@section('title', 'Inbox')
+@section('content')
+    <div class="container">
+    <div class="page-header">
+        <h1>TMail <small>Simple way to destroy your mails :D</small></h1>
     </div>
-    <div class="eachMail">
-        <div class="from">zahra</div>
-        <div class="subject">back to school email</div>
-        <div class="text">Hey! I have to go back the next monday.</div>
-        <div class="date">11:34 95/3/2</div>
+    <div class="row">
+        <div class="col-sm-3 col-md-2">
+            <div class="btn-group">
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+                    Mail <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Mail</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-sm-9 col-md-10">
+            <!-- Split button -->
+            <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    <span class="caret"></span><span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">All</a></li>
+                    <li><a href="#">None</a></li>
+                    <li><a href="#">Read</a></li>
+                    <li><a href="#">Unread</a></li>
+                    <li><a href="#">Starred</a></li>
+                    <li><a href="#">Unstarred</a></li>
+                </ul>
+            </div>
+            <button type="button" class="btn btn-default" data-toggle="tooltip" title="Refresh">
+                <span class="glyphicon glyphicon-refresh"></span>
+            </button>
+            <!-- Single button -->
+            <div class="btn-group">
+                <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+                    More <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Mark all as read</a></li>
+                    <li class="divider"></li>
+                    <li class="text-center"><small class="text-muted">Select messages to see more actions</small></li>
+                </ul>
+            </div>
+            <div class="pull-right">
+                <span class="text-muted"><b>1</b>–<b>50</b> of <b>160</b></span>
+                <div class="btn-group btn-group-sm">
+                    <button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                    </button>
+                    <button type="button" class="btn btn-default">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-3 col-md-2">
+            <a href="#" class="btn btn-danger btn-sm btn-block" role="button"><i class="glyphicon glyphicon-edit"></i> Compose</a>
+            <hr>
+            <ul class="nav nav-pills nav-stacked">
+                <li class="active"><a href="#"><span class="badge pull-right">32</span> Inbox </a>
+                </li>
+                <li><a href="#">Starred</a></li>
+                <li><a href="#">Important</a></li>
+                <li><a href="#">Sent Mail</a></li>
+                <li><a href="#"><span class="badge pull-right">3</span>Drafts</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-md-10">
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs">
+                <li class="active">
+                <a href="#home" data-toggle="tab">
+                    <span class="glyphicon glyphicon-inbox"></span>
+                    &nbspPrimary
+                </a>
+                </li>
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div class="tab-pane fade in active" id="home">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div>
+                            <span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;
+                                display: inline-block;">Mark Otto</span> <span class="">Nice work on the lastest version</span>
+                            <span class="text-muted" style="font-size: 11px;">- More content here</span> <span class="badge">12:10 AM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
+                        </span></span></a><a href="#" class="list-group-item">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div>
+                            <span class="glyphicon glyphicon-star-empty"></span><span class="name" style="min-width: 120px;
+                                display: inline-block;">Jason Markus</span> <span class="">This is big title</span>
+                            <span class="text-muted" style="font-size: 11px;">- I saw that you had..</span> <span class="badge">12:09 AM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
+                        </span></span></a><a href="#" class="list-group-item read">
+                            <div class="checkbox">
+                                <label>
+                                    <input type="checkbox">
+                                </label>
+                            </div>
+                            <span class="glyphicon glyphicon-star"></span><span class="name" style="min-width: 120px;
+                                display: inline-block;">Jane Patel</span> <span class="">This is big title</span>
+                            <span class="text-muted" style="font-size: 11px;">- Hi hello how r u ?</span> <span class="badge">11:30 PM</span> <span class="pull-right"><span class="glyphicon glyphicon-paperclip">
+                        </span></span></a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-</body>
-</html>
-    <script src="./jquery-1.12.0.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            var numOfMail=$("#numOfMail").val();
-            $.ajax({
-                type:"GET",
-                url : "../server.php?inbox=true&nom="+numOfMail,
-                dataType : "xml",
-                cache : false ,
-                async : false ,
-                success : function (xml) {
-                    var all=$(xml).children('mails').children("mail");
-                    all.each(function(){
-                        var email='div class="eachMail">'+'<div class="from">'+$(this).children("from").text();
-                        email+='</div><div class="subject">'+$(this).children("subject").text();
-                        email+='</div><div class="text">'+$(this).children("text").text();
-                        email+='</div><div class="date">'+$(this).children("date").text()+'</div></div>';
-                        $("#mails").append(email);
-                        if($(this).attr("read")!==undefined){//the email has been read
-                            $("mails").children(":last").css('background-color','green');
-                        }else if($(this).attr("spam")!==undefined){
-                            $("mails").children(":last").css('background-color','yellow');
-                        }else{
-                            $("mails").children(":last").css('background-color','white');
-                        }
-                    });
-                    $(document).on('click','.eachMail',function(){
-                        window.location="../server.php?email=true & from="+$(this).children(".from").text()+"& date="+$(this).children(".date").text();
-                    });
-                },error: function(jqXHR, textStatus, errorThrown)
-                         {
-                         // Handle errors here
-                         console.log('ERRORS: ' + textStatus);
-                         console.log(jqXHR.responseText);
-                         // STOP LOADING SPINNER
-                         }
-            });
-
-            $(document).on('click','#refresh',function(){
-                numOfMail=$("#numOfMail").val();
-                $.ajax({
-                type:"GET",
-                url : "../server.php?refresh=true&nom="+numOfMail,
-                dataType : "xml",
-                cache : false ,
-                async : false ,
-                success : function (xml) {
-                    var all=$(xml).children('mails').children("mail");
-                    all.each(function(){
-                        var email='div class="eachMail">'+'<div class="from">'+$(this).children("from").text();
-                        email+='</div><div class="subject">'+$(this).children("subject").text();
-                        email+='</div><div class="text">'+$(this).children("text").text();
-                        email+='</div><div class="date">'+$(this).children("date").text()+'</div></div>';
-                        $("#mails").append(email);
-                        if($(this).attr("read")!==undefined){//the email has been read
-                            $("mails").children(":last").css('background-color','green');
-                        }else if($(this).attr("spam")!==undefined){
-                            $("mails").children(":last").css('background-color','yellow');
-                        }else{
-                            $("mails").children(":last").css('background-color','white');
-                        }
-                    });
-                    $(document).on('click','.eachMail',function(){
-                        window.location="../server.php?email=true & from="+$(this).children(".from").text()+"& date="+$(this).children(".date").text();
-                    });
-                },error: function(jqXHR, textStatus, errorThrown)
-                         {
-                         // Handle errors here
-                         console.log('ERRORS: ' + textStatus);
-                         console.log(jqXHR.responseText);
-                         // STOP LOADING SPINNER
-                         }
-
-                    });
-                });
-
-            $(document).on('click','#compose',function(){
-                        window.location="../server.php?compose=true";
-                    });
-
-            $(document).on('click','#inbox',function(){
-                numOfMail=$("#numOfMail").val();
-                $("#mails").empty();
-                $.ajax({
-                type:"GET",
-                url : "../server.php?inbox=true&nom="+numOfMail,
-                dataType : "xml",
-                cache : false ,
-                async : false ,
-                success : function (xml) {
-                    var all=$(xml).children('mails').children("mail");
-                    all.each(function(){
-                        var email='div class="eachMail">'+'<div class="from">'+$(this).children("from").text();
-                        email+='</div><div class="subject">'+$(this).children("subject").text();
-                        email+='</div><div class="text">'+$(this).children("text").text();
-                        email+='</div><div class="date">'+$(this).children("date").text()+'</div></div>';
-                        $("#mails").append(email);
-                        if($(this).attr("read")!==undefined){//the email has been read
-                            $("mails").children(":last").css('background-color','green');
-                        }else if($(this).attr("spam")!==undefined){
-                            $("mails").children(":last").css('background-color','yellow');
-                        }else{
-                            $("mails").children(":last").css('background-color','white');
-                        }
-                    });
-                    $(document).on('click','.eachMail',function(){
-                        window.location="../server.php?email=true & from="+$(this).children(".from").text()+"& date="+$(this).children(".date").text();
-                    });
-                },error: function(jqXHR, textStatus, errorThrown)
-                         {
-                         // Handle errors here
-                         console.log('ERRORS: ' + textStatus);
-                         console.log(jqXHR.responseText);
-                         // STOP LOADING SPINNER
-                         }
-                    });
-                });
-            $(document).on('click','#sent',function(){
-                numOfMail=$("#numOfMail").val();
-                $("#mails").empty();
-                $.ajax({
-                type:"GET",
-                url : "../server.php?sent=true&nom="+numOfMail,
-                dataType : "xml",
-                cache : false ,
-                async : false ,
-                success : function (xml) {
-
-                    var all=$(xml).children('mails').children("mail");
-                    all.each(function(){
-                        var email='div class="eachMail">'+'<div class="from">'+$(this).children("to").text();
-                        email+='</div><div class="subject">'+$(this).children("subject").text();
-                        email+='</div><div class="text">'+$(this).children("text").text();
-                        email+='</div><div class="date">'+$(this).children("date").text()+'</div></div>';
-                        $("#mails").append(email);
-                        if($(this).attr("read")!==undefined){//the email has been read
-                            $("mails").children(":last").css('background-color','green');
-                        }else if($(this).attr("spam")!==undefined){
-                            $("mails").children(":last").css('background-color','yellow');
-                        }else{
-                            $("mails").children(":last").css('background-color','white');
-                        }
-                    });
-                    $(document).on('click','.eachMail',function(){
-                        window.location="../server.php?email=true & from="+$(this).children(".to").text()+"& date="+$(this).children(".date").text();
-                    });
-                },error: function(jqXHR, textStatus, errorThrown)
-                         {
-                         // Handle errors here
-                         console.log('ERRORS: ' + textStatus);
-                         console.log(jqXHR.responseText);
-                         // STOP LOADING SPINNER
-                         }
-                    });
-                });
-                
-           
-            $('input[type=radio][name=sortby]').change(function() {
-                numOfMail=$("#numOfMail").val();
-                $("#mails").empty();
-                $.ajax({
-                type:"GET",
-                url : "../server.php?inbox=true&sortby="+this.value+"&nom="+numOfMail,
-                dataType : "xml",
-                cache : false ,
-                async : false ,
-                success : function (xml) {
-                    var all=$(xml).children('mails').children("mail");
-                    all.each(function(){
-                        var email='div class="eachMail">'+'<div class="from">'+$(this).children("from").text();
-                        email+='</div><div class="subject">'+$(this).children("subject").text();
-                        email+='</div><div class="text">'+$(this).children("text").text();
-                        email+='</div><div class="date">'+$(this).children("date").text()+'</div></div>';
-                        $("#mails").append(email);
-                        if($(this).attr("read")!==undefined){//the email has been read
-                            $("mails").children(":last").css('background-color','green');
-                        }else if($(this).attr("spam")!==undefined){
-                            $("mails").children(":last").css('background-color','yellow');
-                        }else{
-                            $("mails").children(":last").css('background-color','white');
-                        }
-                    });
-                    $(document).on('click','.eachMail',function(){
-                        window.location="../server.php?email=true & from="+$(this).children(".from").text()+"& date="+$(this).children(".date").text();
-                    });
-                },error: function(jqXHR, textStatus, errorThrown)
-                         {
-                         // Handle errors here
-                         console.log('ERRORS: ' + textStatus);
-                         console.log(jqXHR.responseText);
-                         // STOP LOADING SPINNER
-                         }
-                    });
-            });
-
-        });
-
-
-
-
-    </script>
+@endsection
