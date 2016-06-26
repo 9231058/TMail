f173d1b69d6c58407cdb03b8752a5af2ddf09ca9
Parham Alvani <parham.alvani@gmail.com>
2016-06-27 02:47:23 +0430

    User better CDN for vue.js

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -19,7 +19,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 <!-- Vue.js Thanks to Pooya -->
-<script src="https://cdn.jsdelivr.net/vue/1.0.25/vue.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.25/vue.min.js"></script>
 
 <!-- TMail specific javascripts -->
 <!-- Pages specific javascripts -->
