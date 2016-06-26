b31449a35821529a557f0d67e42ccd57d9038c2c
Parham Alvani <parham.alvani@gmail.com>
2016-06-27 02:46:07 +0430

    Customize our User :)

--- app/User.php
+++ app/User.php
@@ -2,10 +2,20 @@
 
 namespace TMail;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Jenssegers\Mongodb\Eloquent\Model as Model;
 
-class User extends Authenticatable
+class User extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
 {
+    use Authenticatable, Authorizable, CanResetPassword;
     /**
      * The attributes that are mass assignable.
      *
@@ -23,4 +33,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /*
+     * The avatar attribute of user
+     */
+    public function getAvatarAttribute($attr)
+    {
+        if ($attr) {
+            return $attr;
+        } else {
+            return '//www.gravatar.com/avatar/' . md5($this->email).'?d=retro';
+        }
+    }
 }
