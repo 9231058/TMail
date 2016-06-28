58b3796471f7458008f3b7392b42347090b6ce52
Parham Alvani <parham.alvani@gmail.com>
2016-06-29 03:01:52 +0430

    Building user collection in mongo db :D

--- database/migrations/2016_06_20_044746_create_users_table.php
+++ database/migrations/2016_06_20_044746_create_users_table.php
@@ -12,14 +12,8 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('users', function (Blueprint $collection) {
+            $collection->index('email');
         });
     }
 
