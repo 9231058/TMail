5425cc74fa45ea96510698b35cf9b4ef4061bb0f
Parham Alvani <parham.alvani@gmail.com>
2016-06-30 11:17:36 +0430

    User profile page was improved.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,10 +6,8 @@
     <div class="container">
     <div class="page-header">
         <h1>{{$user->first_name}} {{$user->last_name}} <small>{{$user->email}}</small></h1>
-        <div class="col-xs-6 col-md-3">
-            <a href="#" class="thumbnail">
-                <img src="{{$user->avatar}}" alt="Profile Picture">
-            </a>
+        <div class="col-md-3">
+            <img src="{{$user->avatar}}" alt="Profile Picture" class="img-thumbnail">
         </div>
     </div>
 @endsection
