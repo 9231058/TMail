1968f671022b36ca16de0ba0c0eee18ede1a808a
Parham Alvani <parham.alvani@gmail.com>
2016-07-01 03:42:27 +0430

    Inbox javascript was improved.

--- public/js/inbox.js
+++ public/js/inbox.js
@@ -9,11 +9,56 @@
  */
 window.onload = onInboxLoad
 
+$.ajaxSetup({
+  headers: {
+    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+  }
+})
+
+var inbox = new Vue({
+  el: '#home',
+  data: {
+    mails: []
+  }
+})
+
+var compose = new Vue({
+  el: '#compose',
+  data: {
+    hasError: false,
+    errors: {
+      recipient: '',
+      title: '',
+      content: ''
+    }
+  }
+})
+
 function onInboxLoad () {
-  var inbox = new Vue({
-    el: '#home',
-    data: {
-      mails: []
+  $('#compose-recipient').blur(checkMailExistance)
+  $('#compose-send').click(sendMail)
+}
+
+function checkMailExistance () {
+}
+
+function sendMail () {
+  var form = {
+    'recipient': $('#compose-recipient').val(),
+    'title': $('#compose-title').val(),
+    'content': $('#compose-content').val()
+  }
+  $.ajax({
+    type: 'POST',
+    url: '/TMail/mail',
+    data: form,
+    dataType: 'json',
+    success: function (msg) {
+      console.log(msg)
+    },
+    error: function (msg) {
+      compose.hasError = true
+      compose.errors = msg.responseJSON
     }
   })
 }
