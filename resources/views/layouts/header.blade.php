3484657d2e384c7b4723d07de60d0a597ec79163
Parham Alvani <parham.alvani@gmail.com>
2016-06-30 11:10:11 +0430

    Navigation bar was completed.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,10 +7,29 @@
 <meta name="description" content="TMail PHP server">
 
 <title>
-	TMail @yield('title')
+    TMail @yield('title')
 </title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/darkly/bootstrap.min.css" type="text/css">
 
 </head>
 <body style="padding-bottom: 100px;">
+@if (Auth::check())
+    <nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand">
+                <img src="{{Auth::user()->avatar}}" alt="Profile Picture" class="img-circle" height="40px" style="margin: -20% auto;">
+            </a>
+            <a class="navbar-brand" href="{{url('users/'.Auth::user()->id)}}">
+            {{Auth::user()->first_name}} {{Auth::user()->last_name}}
+            </a>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li>{!! link_to('/logout', 'Logout') !!}</li>
+            </ul>
+        </div>
+    </div>
+</nav>
+@endif
