0a981a38857adea8714346d08183330d6a5ffb63
Parham Alvani <parham.alvani@gmail.com>
2016-07-01 03:43:41 +0430

    Mail controller was added.

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * In The Name Of God
+ *
+ * PHP Version 5
+ *
+ * @category HttpController
+ * @package  TMail\Http\Controllers
+ * @author   Parham Alvani <parham.alvani@gmail.com>
+ * @license  http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @link     Link
+ */
+namespace TMail\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+use TMail\Http\Requests;
+use TMail\Mail as Mail;
+
+class MailController extends Controller
+{
+    public function show()
+    {
+    }
+
+    public function index()
+    {
+        $mails = [];
+        if (Auth::check()) {
+        } else {
+            return response()->json($mails);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        if (Auth::check()) {
+            $this->validate($request, [
+                'title' => 'required|max:255',
+                'recipient' => 'required|email|exists:users',
+                'content' => 'required',
+            ]);
+
+            $mail = Mail::create([
+                'title' => $request['title'],
+                'author' => Auth::user()->id,
+                'recipient' => $request['recipient'],
+                'content' => $request['contetn']
+            ]);
+            $mail.save();
+        }
+    }
+}
