9319075d9315daf38bfffef1db2b2dfecb4bf1c7
Parham Alvani <parham.alvani@gmail.com>
2016-06-30 18:08:09 +0430

    Provide some new contact material in user show page.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,7 +2,14 @@
 @section('title')
     {{ $user->email }}
 @endsection
+@section('js')
+    "{{asset('js/userShow.js')}}"
+@endsection
 @section('content')
+    <script>
+        var userId = '{{$user->id}}'
+        var userURL = '{{url('users')}}'
+    </script>
     <div class="container">
     <div class="page-header">
         <h1>{{$user->first_name}} {{$user->last_name}} <small>{{$user->email}}</small></h1>
@@ -10,4 +17,10 @@
             <img src="{{$user->avatar}}" alt="Profile Picture" class="img-thumbnail">
         </div>
     </div>
+    @if (Auth::check())
+    <div class="row">
+        <button type="button" class="btn btn-default" v-if="isContact">Add to Contacts</button>
+        <button type="button" class="btn btn-success" v-else>You are Friends</button>
+    </div>
+    @endif
 @endsection
