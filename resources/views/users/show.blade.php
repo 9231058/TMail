5a634fd7aab77fdae30e61b5602548edf8175c64
Parham Alvani <parham.alvani@gmail.com>
2016-06-30 19:11:18 +0430

    User show page was improved.

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,7 +11,7 @@
         var userURL = '{{url('users')}}'
     </script>
     <div class="container">
-    <div class="page-header">
+    <div class="page-header row">
         <h1>{{$user->first_name}} {{$user->last_name}} <small>{{$user->email}}</small></h1>
         <div class="col-md-3">
             <img src="{{$user->avatar}}" alt="Profile Picture" class="img-thumbnail">
@@ -22,5 +22,23 @@
         <button type="button" class="btn btn-success" v-if="isContact">You are Friends</button>
         <button type="button" class="btn btn-default" v-else id="add-contact">Add to Contacts</button>
     </div>
+    @endif
+    @if (isset($user['contacts']))
+    <h2>Contacts</h2>
+    <table class="table">
+        <tr>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+        </tr>
+        @foreach (($user->contacts) as $contact)
+            <tr>
+                <td>{{TMail\User::find($contact)->email}}</td>
+                <td>{{TMail\User::find($contact)->first_name}}</td>
+                <td>{{TMail\User::find($contact)->last_name}}</td>
+                <td><a href="{{ url('users/'.TMail\User::find($contact)->id) }}" class="btn btn-primary">Go !</a></td>
+            <tr>
+        @endforeach
+    </table>
     @endif
 @endsection
