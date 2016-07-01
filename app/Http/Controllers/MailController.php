d3961b8386b8e01fd1528c5e2c0107fc4452ef87
Parham Alvani <parham.alvani@gmail.com>
2016-07-01 22:32:36 +0430

    Mail controller was improved.

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -20,51 +20,60 @@ use TMail\Mail as Mail;
 
 class MailController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function inbox()
     {
-        if (Auth::check()) {
-            $mails = Mail::where('recipient', Auth::user()->email)
-                ->orderBy('created_at', 'desc')
-                ->paginate(5);
+        $mails = Mail::where('recipient', Auth::user()->email)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
 
-            return response()->json($mails);
-        }
+        return response()->json($mails);
     }
 
     public function sent()
     {
-        if (Auth::check()) {
-            $mails = Mail::where('author', Auth::user()->email)
-                ->orderBy('created_at', 'desc')
-                ->paginate(5);
+        $mails = Mail::where('author', Auth::user()->email)
+            ->orderBy('created_at', 'desc')
+            ->paginate(5);
 
-            return response()->json($mails);
-        }
+        return response()->json($mails);
     }
 
 
     public function store(Request $request)
     {
-        if (Auth::check()) {
-            $this->validate($request, [
-                'title' => 'required|max:255',
-                'recipient' => 'required|email|exists:users,email',
-                'content' => 'required',
-                'attachments' => 'array',
-            ]);
+        $this->validate($request, [
+            'title' => 'required|max:255',
+            'recipient' => 'required|email|exists:users,email',
+            'content' => 'required',
+            'attachments' => 'array',
+        ]);
 
-            $mail = Mail::create([
-                'title' => $request['title'],
-                'author' => Auth::user()->email,
-                'recipient' => $request['recipient'],
-                'content' => $request['content']
-            ]);
-            if (isset($request['attachments'])) {
-                $mail['attachments'] = $request['attachments'];
-            }
-            $mail->save();
-
-            return response()->json($mail);
+        $mail = Mail::create([
+            'title' => $request['title'],
+            'author' => Auth::user()->email,
+            'recipient' => $request['recipient'],
+            'content' => $request['content']
+        ]);
+        if (isset($request['attachments'])) {
+            $mail['attachments'] = $request['attachments'];
         }
+        $mail->save();
+
+        return response()->json($mail);
+    }
+
+    public function destory(Mail $mail)
+    {
+    }
+
+    public function read(Mail $mail)
+    {
+        $mail->readed_at = Carbon\Carbon::now();
+        $mail->save();
     }
 }
